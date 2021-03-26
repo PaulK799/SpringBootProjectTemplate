c61e8173acaa2cf6c89fe1bea7faba4b2ce1eefd
paul.kennedy <paul.kennedy@openjawtech.com>
2021-03-26 16:53:53 +0000

    SBPT-26032021 Add Actuator, make config bean refreshable.

--- src/main/java/com/paulk/demo/config/DemoApplicationConfig.java
+++ src/main/java/com/paulk/demo/config/DemoApplicationConfig.java
@@ -2,10 +2,12 @@ package com.paulk.demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
+@RefreshScope
 public class DemoApplicationConfig {
 
     @Value("${spring.redis.host:localhost}")
