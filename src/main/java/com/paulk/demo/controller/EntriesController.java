e5677c3353be5b94fa3ad3342452e304890015de
paul.kennedy <paul.kennedy@openjawtech.com>
2021-04-06 10:03:33 +0100

    SBPT-06042021 Cleanup code

--- src/main/java/com/paulk/demo/controller/EntriesController.java
+++ src/main/java/com/paulk/demo/controller/EntriesController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
